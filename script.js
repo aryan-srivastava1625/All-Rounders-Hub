9c5b564d3a1f6c33127538a9a6a8b0d50627b277
aryan-srivastava1625 <ap9140@srmist.edu.in>
2024-11-28 14:53:31 +0530

    done with the scroller

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 var crsr = document.querySelector("#cursor")
 var blur1 = document.querySelector("#blurcursor")
 document.addEventListener("mousemove",function(dets){
-    crsr.style.left = dets.x+"px"
+    crsr.style.left = dets.x+30+"px"
     crsr.style.top= dets.y+"px" 
     blur1.style.left = dets.x - 250 + "px"
     blur1.style.top= dets.y - 250 +"px" 
 })
+
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #edb827";
+    crsr.style.backgroundColor = "#95C11E";
+  });
+});
 
 
 
@@ -35,4 +49,19 @@ gsap.to("#main",{
         scrub:2,
     }
 })
+
+gsap.to("#aboutus img,#aboutus1", {
+    y: 90,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#aboutus",
+      scroller: "body",
+      // markers:true,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
+    },
+  });
+  
 
